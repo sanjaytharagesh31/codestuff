5724564b8ac398e1ccf2b2e8a5023fa20cc17e69
sanjaytharagesh <sanjay311999@gmail.com>
2017-04-14 00:07:04 +0530

    Program to delete a substring

---
+++ string/delsubstrin.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i,j,k,len1,len2,pos;
+	char str[100],sub[50];
+	cout<<"Enter a string:";
+	cin>>str;
+	cout<<"Enter a substring to delete from the main string:";
+	cin>>sub;
+	for(len1=0;sub[len1]!='\0';len1++);
+	for(len2=0;sub[len2]!='\0';len2++);
+	
+	for(i=0;str[i]!='\0';i++){
+		if(str[i]==sub[0]){
+			j=i;
+			k=0;
+			while(sub[k]!='\0' && str[j]!='\0'){
+				if(str[j]!=sub[k])
+					break;
+				j++;
+				k++;
+			}
+			if(len2==j-i){
+				break;
+			}
+		}
+	}
+	
+	pos=i;
+	for(i=pos;str[i+3]!='\0';i++)
+		str[i]=str[i+3];
+	cout<<str;
+	return 0;
+}
+	
+	
