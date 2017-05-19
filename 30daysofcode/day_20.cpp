931674b1e2ca9c8e883fe2d4731a7446635c814d
sanjaytharagesh <sanjay311999@gmail.com>
2017-05-19 11:46:01 +0530

    Add day 20 code

---
+++ 30daysofcode/day_20.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n,temp,count;
+	cin >> n;
+	int a[n];
+	for(int a_i = 0; a_i < n; a_i++){
+		cin >> a[a_i];
+	}
+// Write Your Code Here
+	count=0;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n-1;j++){
+			if(a[j]>a[j+1]){
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+				count++;
+			}
+		}
+	}
+	cout<<"Array is sorted in "<<count<<" swaps."<<endl;
+	cout<<"First Element: "<<a[0]<<endl;
+	cout<<"Last Element: "<<a[n-1];
+	return 0;
+}
