7ef98c5796dae154a6010b57788dba41b1c3f4b1
sanjaytharagesh <sanjay311999@gmail.com>
2017-05-26 08:54:45 +0530

    Pattern-1 code upload

---
+++ patterns/design1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Triangle pattern printing"<<endl;
+	for(int i=0;i<10;i++){
+		for(int j=0;j<=i;j++)
+			cout<<"*";
+		cout<<endl;
+	}
+	return 0;
+}	
+	
+	
