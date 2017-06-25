d265029b858f3ec0c1d83730309cc24bc5558322
sanjaytharagesh <sanjay311999@gmail.com>
2017-06-25 15:19:04 +0530

    Add day 27 code

---
+++ 30daysofcode/day_27.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "5" << endl;
+    cout << "5 3" << endl;
+    cout << "4 -4 0 2 4" << endl;
+    cout << "5 3" << endl;
+    cout << "2 -2 4 0 2" << endl;
+    cout << "3 2" << endl;
+    cout << "4 -6 0" << endl;
+    cout << "7 5" << endl;
+    cout << "1 -3 1 1 0 1 1" << endl;
+    cout << "6 4" << endl;
+    cout << "2 -1 4 0 1 1" << endl;
+    return 0;
+}
