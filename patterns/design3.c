fcc48f49b75a0ad03239ced576be2386724c43f4
sanjaytharagesh <sanjay311999@gmail.com>
2018-01-22 19:15:59 +0530

    half diamond pattern

---
+++ patterns/design3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for(int i=1; i<=5; i++) {
+		for(int j=1; j<=i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(int i=n-1; i>=1; i--) {
+		for(int j=1; j<=i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+/*
+
+
+5
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+
+*/
